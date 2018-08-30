b9a46f396c8e95cf8beec0350d03e20f966b9a00
Jonathan Hayes <jonathan.hayes32@outlook.com>
2018-08-30 15:15:30 -0400

    Add project files.

---
+++ Exercise2B/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2B
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            AnimalSound();
+
+        }
+        public static void AnimalSound(string animal = "Cat", string sound = "meow")
+          
+        {
+            Console.WriteLine("what sound does a cat make");
+            Console.ReadLine();
+            Console.WriteLine($"the {animal} goes \"{sound}\" ");
+            Console.ReadLine();
+
+            
+
+        }
+
+
+    }
+
+}
